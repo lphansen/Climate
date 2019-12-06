348a9b2660c1482f5e2ce2230cd26ec774fd0c5c
johnrwilson <johnrwilson@uchicago.edu>
2019-12-06 17:24:43 -0600

    Added old content

---
+++ Preference_appendix/RunMe_Appendix_Competitive.m
@@ -0,0 +1,11 @@
+clear all
+close all
+clc
+
+%%%% This file calls all other scripts for generating results in the
+%%%% appendix that correspond to the competitive model under preference
+%%%% setting. 
+
+run('Competitive_SS')
+run('Competitive_SCC')
+run('Competitive_RE')
