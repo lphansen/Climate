93d7d2fb1664b266c1f128c473c2c0a49f4e8b9c
andyjmwang <jiamingwang@uchicago.edu>
2020-01-02 16:12:38 -0600

    changes according to Han's comments

--- install.bat
+++ install.bat
@@ -9,8 +9,9 @@ echo ===========================================================================
 echo Starting installation process...
 
 echo ===============================================================================
-echo Step 1: Install numba and pybind11
+echo Step 1: Install plotly and pybind11
 pip install pybind11
+pip install plotly
 
 echo ===============================================================================
 echo Step 2: Install model solution core
--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ fi
 echo ===============================================================================
 echo Step 1: Install numba and pybind11
 pip install pybind11
+pip install plotly
 
 echo ===============================================================================
 echo Step 2: Install model solution core
